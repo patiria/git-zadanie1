7d624514dd7136967d910dbf64f7450cfbe0429d
Joanna Lisowska <patiria@poczta.onet.pl>
2019-07-07 16:30:12 +0200

    Updates

---
+++ precache-manifest.65617f872efdc6b22c06bd0e99edff8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2509f5c121c686ec69c3254055cb8f78",
+    "url": "/crypto-rate/index.html"
+  },
+  {
+    "revision": "5f180d590a9157f01f16",
+    "url": "/crypto-rate/static/css/main.293042b0.chunk.css"
+  },
+  {
+    "revision": "e711f691d3cd19de11b4",
+    "url": "/crypto-rate/static/js/2.fcad81e9.chunk.js"
+  },
+  {
+    "revision": "5f180d590a9157f01f16",
+    "url": "/crypto-rate/static/js/main.e5d40af5.chunk.js"
+  },
+  {
+    "revision": "e2a84902c547dfd9bb9c",
+    "url": "/crypto-rate/static/js/runtime~main.bb15693c.js"
+  }
+]);
